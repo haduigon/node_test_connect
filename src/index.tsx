ca7110efcab4c9a2eb68cceb42f3a066e5f32960
haduigon <haduigon@gmail.com>
2023-07-06 23:34:55 +0300

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+
+import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+import App from './App';
+
+// const domNode = document.getElementById('root');
+// const root = createRoot(domNode);
+// console.log(domNode)
+// root.render(<App />);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
